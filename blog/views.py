cab8bae1da8fdd380ce353d18eed12fc81a39812
map1 <524764246@qq.com>
2018-01-28 13:16:41 +0000

    lean sqlite3 models

--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 
 def index(request):
     #return HttpResponse('Hello world!')
-    return render(request, 'index.html',{'hello':{'Hello,Blog2!'}} )
+    article = models.Article.objects.get(pk=1)
+    return render(request, 'blog/index.html',{'article':article} )
