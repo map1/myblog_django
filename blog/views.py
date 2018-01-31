96ab88169978dddbc4e74de6180a7fe57819993e
map1 <524764246@qq.com>
2018-01-31 12:46:25 +0000

    add a new page, blog can direct to article_page

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,9 @@ from . import models
 # Create your views here.
 
 def index(request):
-    #return HttpResponse('Hello world!')
-    article = models.Article.objects.get(pk=1)
-    return render(request, 'blog/index.html',{'article':article} )
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html',{'articles':articles} )
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', {'article':article})
